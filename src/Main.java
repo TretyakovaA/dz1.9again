dab7a005def055dd78efc3e08018e89fae557ccb
TretyakovaA <putinvivo@bk.ru>
2022-06-24 20:06:27 +0300

    pull request want to do

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book book1 = new Book(new Author ("Orlov ", "Anatoliy" ), 100, 2022);
+        Book book2 = new Book( new Author("Vladimir ","Megre"), 150,2020);
+        book2.setYear(1997);
+        System.out.println(book2.getAuthor()+" " + book2.getPage()+" "+ book2.getYear());
+        System.out.println(book1.getAuthor()+" " + book1.getPage()+" "+ book1.getYear());
     }
 }
